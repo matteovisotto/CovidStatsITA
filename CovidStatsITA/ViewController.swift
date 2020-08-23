1ab976839026cc577e2eb682538de8daa5d83400
Matteo Visotto <matteo.visotto@mail.polimi.it>
2020-08-23 07:03:54 +0200

    Initial Commit

---
+++ CovidStatsITA/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CovidStatsITA
+//
+//  Created by Matteo Visotto on 23/08/2020.
+//  Copyright © 2020 Matteo Visotto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
